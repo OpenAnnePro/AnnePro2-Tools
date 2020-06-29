045d71444459f9688253a725401b5e9c162c528a
codetector <codetector@codetector.cn>
2020-06-28 20:51:19 -0400

    Basic Flashing code

---
+++ src/annepro2.rs
@@ -0,0 +1,185 @@
+use rusb::{ConfigDescriptor, DeviceDescriptor, DeviceHandle, DeviceList, EndpointDescriptor, InterfaceDescriptor, Language, Result, Speed, UsbContext, Device, GlobalContext, Interface};
+use std::time::Duration;
+use std::intrinsics::transmute;
+use std::panic::resume_unwind;
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+pub enum AP2Target {
+    UsbHost = 1,
+    BleHost = 2,
+    McuMain = 3,
+    McuLed = 4,
+    McuBle = 5,
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+pub enum L2Command {
+    GLOBAL = 1,
+    FW = 2,
+    KEYBOARD = 16,
+    LED = 32,
+    MACRO = 48,
+    BLE = 64,
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone)]
+pub enum KeyCommand {
+    Reserved = 0,
+    IapMode = 1,
+    IapGetMode = 2,
+    IapGetFwVersion = 3,
+    IapWirteMemory = 49,
+    // 0x31
+    IapWriteApFlag = 50,
+    // 0x32
+    IapEraseMemory = 67, // 0x43
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum AP2FlashError {
+    NoDeviceFound,
+    MultipleDeviceFound,
+    USBError,
+    EraseError,
+    FlashError,
+    OtherError,
+}
+
+const USB_TIMEOUT: Duration = Duration::from_secs(1);
+
+pub fn flash_firmware<R: std::io::Read>(target: AP2Target, base: u32, file: &mut R, vid: u16, pid: u16) -> std::result::Result<(), AP2FlashError> {
+    let devices = DeviceList::new().map_err(|_| { AP2FlashError::OtherError })?;
+
+    let mut filtered_devices: Vec<Device<GlobalContext>> = devices.iter()
+        .filter(|dev| {
+            if let Ok(desc) = dev.device_descriptor() {
+                return desc.vendor_id() == vid && desc.product_id() == pid;
+            }
+            return false;
+        }).collect();
+
+    println!("{} devices found.", filtered_devices.len());
+    if filtered_devices.is_empty() {
+        return Err(AP2FlashError::NoDeviceFound);
+    }
+    if filtered_devices.len() > 1 {
+        return Err(AP2FlashError::MultipleDeviceFound);
+    }
+    let dev = filtered_devices.pop().expect("has dev");
+    let dev_desc = dev.device_descriptor().expect("has desc");
+
+    println!("Found Anne Pro 2 In IAP Mode on Bus {:03} Port {:03} vid:pid {:04x}:{:04x}",
+             dev.bus_number(), dev.port_number(), dev_desc.vendor_id(), dev_desc.product_id());
+
+    println!("Device has {} configuration(s), using the first one", dev_desc.num_configurations());
+
+    let config_desc = dev.config_descriptor(0).expect("no config");
+
+    let mut dev_handle = dev.open().expect("can't open handle");
+    let lang = dev_handle.read_languages(USB_TIMEOUT).map_err(|_| { AP2FlashError::USBError })?[0];
+
+    let dev_name = dev_handle.read_product_string(lang, &dev_desc, USB_TIMEOUT)
+        .map_err(|_| { AP2FlashError::USBError })?;
+    let dev_serial = dev_handle.read_serial_number_string(lang, &dev_desc, USB_TIMEOUT)
+        .map_err(|_| { AP2FlashError::USBError })?;
+    println!("Device has name: \"{}\" Serial: {}", dev_name, dev_serial);
+    println!("has {} interfaces. We use second interface and EP 3/4", config_desc.num_interfaces());
+    if config_desc.num_interfaces() < 2 {
+        return Err(AP2FlashError::USBError);
+    }
+
+    let mut interfaces: Vec<Interface> = config_desc.interfaces().collect();
+    let interface = &mut interfaces[1];
+
+    let ifdesc = interface.descriptors().next().unwrap();
+    println!("Found {} eps on if#1", ifdesc.num_endpoints());
+    let mut epdesc: Vec<EndpointDescriptor> = ifdesc.endpoint_descriptors().collect();
+    println!("{:#?}", epdesc);
+
+    dev_handle.detach_kernel_driver(1);
+    dev_handle.claim_interface(1).expect("claim");
+
+    // Flashing Code
+    erase_device(&dev_handle, epdesc[0].address(), target, base).map_err(|_| { AP2FlashError::EraseError })?;
+    flash_file(&dev_handle, epdesc[0].address(), target, base, file);
+
+
+    dev_handle.release_interface(1).expect("claim");
+    dev_handle.attach_kernel_driver(1);
+
+    Ok(())
+}
+
+pub fn flash_file<T: UsbContext, F: std::io::Read>(handle: &DeviceHandle<T>, ep: u8,
+                                                   target: AP2Target, base: u32, file: &mut F) {
+    let chunk_size = match &target {
+        AP2Target::McuBle => 32usize,
+        _ => 48usize,
+    };
+    let mut current_addr = base;
+    loop {
+        let mut buffer = vec![0u8; chunk_size];
+        let size = file.read(&mut buffer).expect("read file failure");
+
+        if size > 0 {
+            let result = write_chunk(handle, ep, target, current_addr, &buffer);
+            if result.is_err() {
+                println!("[WARNING] Error {:?} occurred during write at {:#08x}, continuing...",
+                         result.unwrap_err(), current_addr);
+            } else {
+                println!("[INFO] Wrote {} bytes, at {:#08x}, total: {} bytes written",
+                         size, current_addr, (current_addr + size as u32) - base);
+            }
+            current_addr += size as u32;
+        }
+
+        if size < chunk_size {
+            break;
+        }
+    }
+}
+
+pub fn write_chunk<T: UsbContext>(handle: &DeviceHandle<T>, ep: u8, target: AP2Target, addr: u32, chunk: &[u8]) -> Result<()> {
+    let mut buffer: Vec<u8> = Vec::new();
+    buffer.push(L2Command::FW as u8);
+    buffer.push(KeyCommand::IapWirteMemory as u8);
+    let addr_slice: [u8; 4] = unsafe { transmute(addr.to_le()) };
+    buffer.extend_from_slice(&addr_slice);
+    buffer.extend_from_slice(chunk);
+    write_to_target(handle, ep, target, &buffer).map(|_| { () })
+}
+
+pub fn erase_device<T: UsbContext>(handle: &DeviceHandle<T>, ep: u8, target: AP2Target, addr: u32) -> Result<()> {
+    let mut buffer: Vec<u8> = Vec::new();
+    buffer.push(L2Command::FW as u8);
+    buffer.push(KeyCommand::IapEraseMemory as u8);
+    let addr_slice: [u8; 4] = unsafe { transmute(addr.to_le()) };
+    buffer.extend_from_slice(&addr_slice);
+
+    write_to_target(handle, ep, target, &buffer).map(|_| { () })
+}
+
+pub fn write_to_target<T: UsbContext>(handle: &DeviceHandle<T>, ep: u8, target: AP2Target, payload: &[u8]) -> Result<usize> {
+    let mut buffer: Vec<u8> = Vec::with_capacity(64);
+    buffer.push(0x7b);
+    buffer.push(0x10);
+    buffer.push((((target as u8) & 0xF) << 4) | AP2Target::UsbHost as u8);
+    buffer.push(0x10);
+    buffer.push(payload.len() as u8);
+    buffer.push(0);
+    buffer.push(0);
+    buffer.push(0x7d);
+    buffer.extend_from_slice(payload);
+    if buffer.len() > 64 {
+        panic!("Wut?");
+    }
+    // Pad to 64 bytes
+    while buffer.len() < 64 {
+        buffer.push(0);
+    }
+
+    handle.write_interrupt(ep, &buffer, USB_TIMEOUT)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,243 +1,49 @@
-use rusb::{
-    ConfigDescriptor, DeviceDescriptor, DeviceHandle, DeviceList, EndpointDescriptor,
-    InterfaceDescriptor, Language, Result, Speed, UsbContext,
-};
-use std::time::Duration;
+use structopt::StructOpt;
+use std::num::ParseIntError;
+use crate::annepro2::AP2Target;
+use std::path::PathBuf;
+use std::fs::File;
 
-struct UsbDevice<T: UsbContext> {
-    handle: DeviceHandle<T>,
-    language: Language,
-    timeout: Duration,
+pub mod annepro2;
+
+fn parse_hex_16(src: &str) -> std::result::Result<u16, ParseIntError> {
+    if src.starts_with("0x") {
+        u16::from_str_radix(&src[2..], 16)
+    } else {
+        u16::from_str_radix(src, 16)
+    }
+}
+
+fn parse_hex(src: &str) -> std::result::Result<u32, ParseIntError> {
+    if src.starts_with("0x") {
+        u32::from_str_radix(&src[2..], 16)
+    } else {
+        u32::from_str_radix(src, 16)
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "annepro2_tools")]
+struct ArgOpts {
+    #[structopt(short="v", long="vid", parse(try_from_str = parse_hex_16), default_value = "04d9")]
+    vid: u16,
+    #[structopt(short="p", long="pid", parse(try_from_str = parse_hex_16), default_value = "8008")]
+    pid: u16,
+    #[structopt(long, parse(try_from_str = parse_hex), default_value = "0x4000")]
+    base: u32,
+    /// File to be flashed onto device
+    #[structopt(name = "file", parse(from_os_str))]
+    file: PathBuf,
 }
 
 fn main() {
-    list_devices().unwrap();
-}
-
-fn list_devices() -> Result<()> {
-    let timeout = Duration::from_secs(1);
-
-    for device in DeviceList::new()?.iter() {
-        let device_desc = match device.device_descriptor() {
-            Ok(d) => d,
-            Err(_) => continue,
-        };
-
-        let mut usb_device = {
-            match device.open() {
-                Ok(h) => match h.read_languages(timeout) {
-                    Ok(l) => {
-                        if l.len() > 0 {
-                            Some(UsbDevice {
-                                handle: h,
-                                language: l[0],
-                                timeout,
-                            })
-                        } else {
-                            None
-                        }
-                    }
-                    Err(_) => None,
-                },
-                Err(_) => None,
-            }
-        };
-
-        println!(
-            "Bus {:03} Device {:03} ID {:04x}:{:04x} {}",
-            device.bus_number(),
-            device.address(),
-            device_desc.vendor_id(),
-            device_desc.product_id(),
-            get_speed(device.speed())
-        );
-        print_device(&device_desc, &mut usb_device);
-
-        for n in 0..device_desc.num_configurations() {
-            let config_desc = match device.config_descriptor(n) {
-                Ok(c) => c,
-                Err(_) => continue,
-            };
-
-            print_config(&config_desc, &mut usb_device);
-
-            for interface in config_desc.interfaces() {
-                for interface_desc in interface.descriptors() {
-                    print_interface(&interface_desc, &mut usb_device);
-
-                    for endpoint_desc in interface_desc.endpoint_descriptors() {
-                        print_endpoint(&endpoint_desc);
-                    }
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn print_device<T: UsbContext>(device_desc: &DeviceDescriptor, handle: &mut Option<UsbDevice<T>>) {
-    println!("Device Descriptor:");
-    println!(
-        "  bcdUSB             {:2}.{}{}",
-        device_desc.usb_version().major(),
-        device_desc.usb_version().minor(),
-        device_desc.usb_version().sub_minor()
-    );
-    println!("  bDeviceClass        {:#04x}", device_desc.class_code());
-    println!(
-        "  bDeviceSubClass     {:#04x}",
-        device_desc.sub_class_code()
-    );
-    println!("  bDeviceProtocol     {:#04x}", device_desc.protocol_code());
-    println!("  bMaxPacketSize0      {:3}", device_desc.max_packet_size());
-    println!("  idVendor          {:#06x}", device_desc.vendor_id());
-    println!("  idProduct         {:#06x}", device_desc.product_id());
-    println!(
-        "  bcdDevice          {:2}.{}{}",
-        device_desc.device_version().major(),
-        device_desc.device_version().minor(),
-        device_desc.device_version().sub_minor()
-    );
-    println!(
-        "  iManufacturer        {:3} {}",
-        device_desc.manufacturer_string_index().unwrap_or(0),
-        handle.as_mut().map_or(String::new(), |h| h
-            .handle
-            .read_manufacturer_string(h.language, device_desc, h.timeout)
-            .unwrap_or(String::new()))
-    );
-    println!(
-        "  iProduct             {:3} {}",
-        device_desc.product_string_index().unwrap_or(0),
-        handle.as_mut().map_or(String::new(), |h| h
-            .handle
-            .read_product_string(h.language, device_desc, h.timeout)
-            .unwrap_or(String::new()))
-    );
-    println!(
-        "  iSerialNumber        {:3} {}",
-        device_desc.serial_number_string_index().unwrap_or(0),
-        handle.as_mut().map_or(String::new(), |h| h
-            .handle
-            .read_serial_number_string(h.language, device_desc, h.timeout)
-            .unwrap_or(String::new()))
-    );
-    println!(
-        "  bNumConfigurations   {:3}",
-        device_desc.num_configurations()
-    );
-}
-
-fn print_config<T: UsbContext>(config_desc: &ConfigDescriptor, handle: &mut Option<UsbDevice<T>>) {
-    println!("  Config Descriptor:");
-    println!(
-        "    bNumInterfaces       {:3}",
-        config_desc.num_interfaces()
-    );
-    println!("    bConfigurationValue  {:3}", config_desc.number());
-    println!(
-        "    iConfiguration       {:3} {}",
-        config_desc.description_string_index().unwrap_or(0),
-        handle.as_mut().map_or(String::new(), |h| h
-            .handle
-            .read_configuration_string(h.language, config_desc, h.timeout)
-            .unwrap_or(String::new()))
-    );
-    println!("    bmAttributes:");
-    println!("      Self Powered     {:>5}", config_desc.self_powered());
-    println!("      Remote Wakeup    {:>5}", config_desc.remote_wakeup());
-    println!("    bMaxPower           {:4}mW", config_desc.max_power());
-
-    if let Some(extra) = config_desc.extra() {
-        println!("    {:?}", extra);
+    let args: ArgOpts = ArgOpts::from_args();
+    println!("args: {:#x?}", args);
+    let mut file = File::open(args.file).expect("invalid file");
+    let result = annepro2::flash_firmware(AP2Target::McuMain, args.base, &mut file, args.vid, args.pid);
+    if result.is_ok() {
+        println!("Flash complete");
     } else {
-        println!("    no extra data");
-    }
-}
-
-fn print_interface<T: UsbContext>(
-    interface_desc: &InterfaceDescriptor,
-    handle: &mut Option<UsbDevice<T>>,
-) {
-    println!("    Interface Descriptor:");
-    println!(
-        "      bInterfaceNumber     {:3}",
-        interface_desc.interface_number()
-    );
-    println!(
-        "      bAlternateSetting    {:3}",
-        interface_desc.setting_number()
-    );
-    println!(
-        "      bNumEndpoints        {:3}",
-        interface_desc.num_endpoints()
-    );
-    println!(
-        "      bInterfaceClass     {:#04x}",
-        interface_desc.class_code()
-    );
-    println!(
-        "      bInterfaceSubClass  {:#04x}",
-        interface_desc.sub_class_code()
-    );
-    println!(
-        "      bInterfaceProtocol  {:#04x}",
-        interface_desc.protocol_code()
-    );
-    println!(
-        "      iInterface           {:3} {}",
-        interface_desc.description_string_index().unwrap_or(0),
-        handle.as_mut().map_or(String::new(), |h| h
-            .handle
-            .read_interface_string(h.language, interface_desc, h.timeout)
-            .unwrap_or(String::new()))
-    );
-
-    if let Some(extra) = interface_desc.extra() {
-        println!("    {:?}", extra);
-    } else {
-        println!("    no extra data");
-    }
-}
-
-fn print_endpoint(endpoint_desc: &EndpointDescriptor) {
-    println!("      Endpoint Descriptor:");
-    println!(
-        "        bEndpointAddress    {:#04x} EP {} {:?}",
-        endpoint_desc.address(),
-        endpoint_desc.number(),
-        endpoint_desc.direction()
-    );
-    println!("        bmAttributes:");
-    println!(
-        "          Transfer Type          {:?}",
-        endpoint_desc.transfer_type()
-    );
-    println!(
-        "          Synch Type             {:?}",
-        endpoint_desc.sync_type()
-    );
-    println!(
-        "          Usage Type             {:?}",
-        endpoint_desc.usage_type()
-    );
-    println!(
-        "        wMaxPacketSize    {:#06x}",
-        endpoint_desc.max_packet_size()
-    );
-    println!(
-        "        bInterval            {:3}",
-        endpoint_desc.interval()
-    );
-}
-
-fn get_speed(speed: Speed) -> &'static str {
-    match speed {
-        Speed::Super => "5000 Mbps",
-        Speed::High => " 480 Mbps",
-        Speed::Full => "  12 Mbps",
-        Speed::Low => " 1.5 Mbps",
-        Speed::Unknown => "(unknown)",
+        println!("Flash error: {:?}", result.unwrap_err());
     }
 }
